29c10ba759a3b855e6698a9fe7bef5fd2107fca7
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2023-08-02 00:25:21 -0400

    Final Solution

--- script.js
+++ script.js
@@ -2,16 +2,35 @@
 let pokedexContainer = document.querySelector('#pokedex-container');
 
 for (let pokemon of pokedex) {
-  /* Create a Div for Each Pokemon and Give it a Class of Pokemon
-  This pokemonDiv will hold every element we add */
+  // Create a Div for Each Pokemon and Give it a Class of Pokemon
+  let pokemonDiv = document.createElement('div');
+  pokemonDiv.setAttribute('class', 'pokemon');
+
   /* Let's grab each piece of information and make an element for it */
-  // Name -> Make an h1 and add the text, give it a class of 'name'
-  // Append to pokemonDiv
-  // Type -> make pokemonTypes div and add each type to it
+  // Name
+  let pokemonName = document.createElement('h1');
+  pokemonName.setAttribute('class', 'name');
+  pokemonName.innerHTML = pokemon.name.english;
+  pokemonDiv.appendChild(pokemonName);
+
+  // Type
+  let pokemonTypes = document.createElement('div');
+  pokemonTypes.setAttribute('class', 'types');
+
   // Loop through each type in the type array (even if it's one it'll still be an array)
-  // Append to pokemonTypes
+  for (let type of pokemon.type) {
+    let pokemonType = document.createElement('p');
+    pokemonType.innerHTML = type;
+
+    // Append to pokemonTypes
+    pokemonTypes.appendChild(pokemonType);
+  }
+
   // Append all types (pokemonTypes) to pokemonDiv
+  pokemonDiv.appendChild(pokemonTypes);
+
   // Append the pokemonDiv to pokedexContainer
+  pokedexContainer.appendChild(pokemonDiv);
 }
 
 /* EXTRA CREDIT: 
